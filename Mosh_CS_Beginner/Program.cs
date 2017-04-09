e85b5957ee34817801ba047b73944db38c8aadf4
RM-Terrell <terrellrmr@gmail.com>
2017-04-09 17:45:04 -0600

    Add project files.

---
+++ Mosh_CS_Beginner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh_CS_Beginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
