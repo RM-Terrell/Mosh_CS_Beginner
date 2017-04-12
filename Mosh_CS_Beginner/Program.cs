9dddc32ed20fba88ebb24aab1487bcb48c9980a7
RM-Terrell <terrellrmr@gmail.com>
2017-04-11 21:57:47 -0600

    type notes

--- Mosh_CS_Beginner/Program.cs
+++ Mosh_CS_Beginner/Program.cs
@@ -10,7 +10,16 @@ namespace Mosh_CS_Beginner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            //Console.WriteLine("Hello world!");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f; //have to add f to explicitly treat as float
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = true;
+
+
+            Console.WriteLine(number);
         }
     }
 }
