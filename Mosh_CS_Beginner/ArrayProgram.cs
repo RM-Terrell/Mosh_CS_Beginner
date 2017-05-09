67db586bafa25bbb270302df79b76a13f21e73e0
RM-Terrell <terrellrmr@gmail.com>
2017-05-09 15:24:56 -0600

    strings and arrays

---
+++ Mosh_CS_Beginner/ArrayProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mosh_CS_Beginner
+{
+    class ArrayProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3]; //Need to specifiy size as 3. Can also use "var" instead of int[]
+            numbers[0] = 1; // default values in a number array are 0. Default values in a bool array are false ( 0 is false 1 is true)
+
+        }
+
+    }
+}
---
+++ Mosh_CS_Beginner/Strings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// string name = string.Format("{0} {1}", firstName, lastName); as a way to format strings
+
+// Strings are immutable
+//   \n new ling
+//   \tab tab
+//   \\ backslash
+//   \' single quote
+//   \" double quote
+
+namespace Mosh_CS_Beginner
+{
+    class Strings
+    {
+        static void Main(string[] args)
+        {
+            int number; // note "var" types on this code and below
+            //same as
+            Int32 number2;
+
+            var firstName = "Mosh";
+
+            String lastName = "Hamedani"; // Need using System for this
+            //Same as
+            string myName = "Mosh";
+
+
+        }
+
+    }
+}
