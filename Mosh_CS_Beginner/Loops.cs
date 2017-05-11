c15d84cb263b433be633515126da9ce4b1e51a54
RM-Terrell <terrellrmr@gmail.com>
2017-05-10 21:16:45 -0600

    control flow

--- Mosh_CS_Beginner/Loops.cs
+++ Mosh_CS_Beginner/Loops.cs
@@ -10,7 +10,66 @@ namespace Mosh_CS_Beginner
     {
         static void Main(string[] args)
         {
+            for(var i = 0; i <= 10; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
 
+            // Reverse the loop
+            for(var i = 10; i >= 1; i--)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+
+            // While loop verison
+            var x = 0;
+            while(x <= 10)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine(x);
+                }
+                x++;
+
+            }
+
+
+            // Display each element of a string
+            var name = "John Smith";
+            //for(var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //For each version
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            // Name echo while loop
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break; //jumps out of the loops
+
+                Console.WriteLine("@EchoL " + input);
+            }
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
