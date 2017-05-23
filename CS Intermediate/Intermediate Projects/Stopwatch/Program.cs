34773f70b4c4c9ac688fa3fb2a798972f7f47acd
RM-Terrell <terrellrmr@gmail.com>
2017-05-23 14:26:11 -0600

    Stop watch project

---
+++ CS Intermediate/Intermediate Projects/Stopwatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+/*Design a class called Stopwatch. The job of this class is to simulate a stopwatch. It should
+provide two methods: Start and Stop. We call the start method first, and the stop method next.
+Then we ask the stopwatch about the duration between start and stop. Duration should be a
+value in TimeSpan. Display the duration on the console.
+
+We should also be able to use a stopwatch multiple times. So we may start and stop it and then
+start and stop it again. Make sure the duration value each time is calculated properly.
+We should not be able to start a stopwatch twice in a row (because that may overwrite the initial
+start time). So the class should throw an InvalidOperationException if its started twice.*/
+
+    /*Personal notes: Decided to break this into two files in order to keep things more organized and practice using common namespaces and calling methods, etc.*/
+
+namespace CS_Intermediate.Intermediate_Projects.Stopwatch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                Console.WriteLine("Press enter to start stopwatch. Press escape twice to stop program.");
+
+                var keyPressed = Console.ReadKey().Key;
+                var stopWatch = new Stopwatch();
+
+                stopWatch.Start(DateTime.Now);
+
+                Console.WriteLine("Press any key to stop stopwatch");
+                keyPressed = Console.ReadKey().Key;
+
+                if (keyPressed.Equals(ConsoleKey.Escape)) break;
+
+                stopWatch.Stop(DateTime.Now);
+
+                Console.WriteLine("Stop - start = {0}\n", stopWatch.Difference());
+                
+                
+            }
+        }
+    }
+}
---
+++ CS Intermediate/Intermediate Projects/Stopwatch/StopWatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CS_Intermediate.Intermediate_Projects.Stopwatch
+{
+
+    class Stopwatch
+    {
+        private bool _isRunning;
+        private DateTime _startTime;
+        private DateTime _stopTime;
+
+        public Stopwatch()
+        {
+            _isRunning = false;
+        }
+
+        public void Start(DateTime start) // Method for starting
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Start"); // Error code for starting twice
+
+            _isRunning = true;
+            _startTime = start;
+        }
+
+        public void Stop(DateTime stop)
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("Stop");
+
+            _isRunning = false;
+            _stopTime = stop;
+        }
+
+        public TimeSpan Difference()
+        {
+            return _stopTime - _startTime;
+        }
+    }
+}
